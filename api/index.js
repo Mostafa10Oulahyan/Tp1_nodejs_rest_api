8d8c0c04438c8ac4128186987f0104f03759a079
Mostafa10Oulahyan <mostaphaoulahyan@gmail.com>
2026-02-20 11:53:44 +0000

    dd

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const { success } = require("../functions");   // <-- chemin important
+const { success } = require("/functions");   // <-- chemin important
 
 const app = express();
 
