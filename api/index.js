2ce6f66a9a5347bffa07857a802c60bb91600440
Mostafa10Oulahyan <mostaphaoulahyan@gmail.com>
2026-02-21 17:17:29 +0000

    pss

--- api/index.js
+++ api/index.js
@@ -12,6 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
